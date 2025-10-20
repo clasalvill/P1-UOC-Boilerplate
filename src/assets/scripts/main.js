64d0e5b2d018b675e334be3804c56d112587afae
Clara Salvador <clasalvill@gmail.com>
2025-10-20 21:57:08 +0200

    Reconecta proyecto y corrige build

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,10 @@
+// Importa SCSS principal para que Parcel genere CSS
+import "../styles/main.scss";
+
+// Código JS adicional
+console.log("UOC Boilerplate funcionando");
+
+(function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
